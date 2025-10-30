0c491ee8af25714c3dc41d5bba263543852ed9cb
Thales Calil <thaleshcalil@gmail.com>
2025-10-30 11:24:29 -0300

    feat: envia projeto inicial Spring Boot

---
+++ SistemaEscola/src/test/java/org/escola/escolaspringboot/EscolaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.escola.escolaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
